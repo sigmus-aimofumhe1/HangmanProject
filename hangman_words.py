38a6b942f5e5cb9707066ec84a2005a34e4706b9
Sigmus Aimofumhe <sigmus_aimofumhe@MacBook-Air.local>
2024-07-28 10:54:29 -0400

    Initial commit

---
+++ hangman_words.py
@@ -0,0 +1,5 @@
+word_list = ["hangman", "chairs", "backpack", "bodywash", "clothing",
+                 "computer", "python", "program", "glasses", "sweatshirt",
+                 "sweatpants", "mattress", "friends", "clocks", "biology",
+                 "algebra", "suitcase", "knives", "ninjas", "shampoo", "baboon",
+             "tailor", "mantle"]
